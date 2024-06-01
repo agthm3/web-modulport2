ee9555eebb955ec0510dd418116c33eb22555093
agthm3 <gigam57@gmail.com>
2024-06-01 17:09:11 +0800

    add seeder for admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use UserSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +16,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::create([
+            'email' => 'admin@mail.com',
+            'name' => 'administrator',
+            'password' => Hash::make('admin123'),
+            'role' => 'Admin',
+            'last_login' => now(),
+        ]);
     }
 }
