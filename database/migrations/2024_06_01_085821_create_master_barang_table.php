0a58d31e0222247d94e498a4b57a13fd379d8a04
agthm3 <gigam57@gmail.com>
2024-06-01 16:59:23 +0800

    add migration for barang and pembelian, modify user migration

--- database/migrations/2024_06_01_085821_create_master_barang_table.php
+++ database/migrations/2024_06_01_085821_create_master_barang_table.php
@@ -11,22 +11,21 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('email')->unique();
-            $table->string('name');
-            $table->string('password');
-            $table->enum('role', ['Admin', 'User']);
-            $table->timestamp('last_login')->nullable();
+        Schema::create('master_barang', function (Blueprint $table) {
+            $table->string('kode_barang')->primary();
+            $table->string('nama_barang');
+            $table->string('satuan');
+            $table->integer('qty');
+            $table->decimal('harga', 15, 2);
             $table->timestamps();
         });
     }
-
+    
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('master_barang');
     }
 };
